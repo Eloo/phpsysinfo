ee2fdd34700bd84431bb4a1f1b3c3ed47e892ebe
Joseph Weigl <joseph.weigl@gmail.com>
2014-06-22 15:02:36 +0200

    test df only for i-data

--- includes/xml/class.XML.inc.php
+++ includes/xml/class.XML.inc.php
@@ -350,8 +350,10 @@ class XML
         $fs = $this->_xml->addChild('FileSystem');
         foreach ($this->_sys->getDiskDevices() as $disk) {
             if (!in_array($disk->getMountPoint(), $hideMounts, true) && !in_array($disk->getFsType(), $hideFstypes, true) && !in_array($disk->getName(), $hideDisks, true)) {
-                $mount = $fs->addChild('Mount');
-                $this->_fillDevice($mount, $disk, $i++);
+                if(startsWith($disk->getMountPoint(), "/i-data/"){
+                  $mount = $fs->addChild('Mount');
+                    $this->_fillDevice($mount, $disk, $i++);
+                }
             }
         }
     }
